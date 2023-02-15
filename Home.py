92c0e670ec24877b1609145a855938a2e6fab488
Idan-Levy27 <73771891+Idan-Levy27@users.noreply.github.com>
2023-02-15 15:20:04 +0200

    update data

--- Home.py
+++ Home.py
@@ -12,14 +12,10 @@ with col1:
 
 with col2:
     st.title("Idan Levy")
-    content = """Hi, I am Idan Levy! I am an experienced developer with a strong background in developing and optimizing algorithms through large code bases in C#, Python, and Java.
-                 I am proficient in using various source code control tools such as Git and Github,
-                 which allows me to work in a team environment more efficiently.
-                 Additionally, I possess a strong knowledge in data analysis and statistical models to predict outcomes.
-                 My fast self-learning ability and excellent interpersonal skills have enabled me to succeed in various collaborative settings,
-                 which I always find to be the most rewarding experiences.
-                 My high ambition to thrive in a team environment makes me a great team player, 
-                 and I'm always ready to learn and take on new challenges."""
+    content = """Hi, I am Idan Levy! I am experienced developer with a background in developing and optimizing algorithms 
+    using C#, Python, and Java. Also skilled in data analysis and statistical modeling to predict outcomes. 
+    I have an excellent interpersonal skills, a fast self-learning ability, and are highly ambitious to thrive in a team environment.
+    Always ready to learn and take on new challenges."""
     st.info(content)
 
 empty_1, center_col, empty_2 = st.columns([1, 4, 1])
